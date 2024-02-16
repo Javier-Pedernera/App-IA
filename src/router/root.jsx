765d1e45673c1bb87ab282be186a173333acb188
JaviPeder~ <javierpedernera@gmail.com>
2024-02-15 21:41:08 -0300

    cambio en reproduccion handleSpeech_useEffect

--- src/router/root.jsx
+++ src/router/root.jsx
@@ -1,8 +1,30 @@
-import { createBrowserRouter } from "react-router-dom";
-// import PublicRoute from "./public_route";
-import PrivateRoute from "./private_route";
+import Home from "../components/Home/Home.jsx"
+import Landing from "../components/Landing/index.jsx";
+import Layout from "../components/Layout/index.jsx";
+import FAQPage from "../components/FAQPage/index.jsx";
+import { Navigate } from "react-router-dom";
 
 export const router = createBrowserRouter([
-  PrivateRoute(),
+  {
+    element: <Layout />,
+    children:[
+        {
+            path: "/landing",
+            element: <Landing />
+        },
+        {
+            path: "/home/:storedThreadId",
+            element: <Home />
+        },
+        {
+            path: '/faq',
+            element: <FAQPage />
+        },
+        {
+            path: "*",
+            element: <Navigate to="/landing" replace />
+        }
+    ]
+}
   // ...PublicRoute(),
 ]);
